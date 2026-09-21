a332f2fdfc6e3002f6f41e70a0280d12b4f48160
osama <ososarafat88@gmail.com>
2026-09-21 20:42:47 +0300

    Add Dividing function

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -10,7 +10,10 @@ int Subtract(int number1, int number2)
 {
     return number1 - number2;
 }
-
+int Divide(int number1, int number2)
+{
+    return number1 / number2;
+}
 int main()
 {
     cout << "10 + 5 = " << Add(10, 5) << endl;
