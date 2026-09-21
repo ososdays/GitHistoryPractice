adfa67e3cb673b18f861745cc9bbf76bdb8a5a93
osama <ososarafat88@gmail.com>
2026-09-21 20:37:05 +0300

    Add project files.

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int Add(int number1, int number2)
+{
+    return number1 + number2;
+}
+
+int Subtract(int number1, int number2)
+{
+    return number1 - number2;
+}
+
+int main()
+{
+    cout << "10 + 5 = " << Add(10, 5) << endl;
+    cout << "10 - 5 = " << Subtract(10, 5) << endl;
+
+    return 0;
+}
